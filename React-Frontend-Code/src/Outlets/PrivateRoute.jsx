459908eac42bdf9362f05d98dc1dc26534cf57a5
Rohit <158751557+rohitindustry09@users.noreply.github.com>
2025-03-05 22:14:20 +0530

    Update PrivateRoute.jsx

--- React-Frontend-Code/src/Outlets/PrivateRoute.jsx
+++ React-Frontend-Code/src/Outlets/PrivateRoute.jsx
@@ -1,22 +1,13 @@
 import { Navigate } from "react-router-dom";
-import { useState, useEffect } from "react";
 
 const getUser = () => {
   return localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null;
 };
 
 const PrivateRoute = ({ children, redirectTo }) => {
-  const [loading, setLoading] = useState(true);
   const user = getUser(); 
 
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 500); // Simulating auth check delay
-  }, []);
-
-  if (loading) return <div>Loading...</div>; // ✅ Prevents flickering
-  return user ? children : <Navigate to={redirectTo} />;
+  return user ? children : <Navigate to={redirectTo} replace />;
 };
 
 export default PrivateRoute;
