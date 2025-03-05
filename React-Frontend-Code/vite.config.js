d89200aeccf790a849d1df3c171a24bed35c9cf1
Rohit <158751557+rohitindustry09@users.noreply.github.com>
2025-03-05 21:31:02 +0530

    Update vite.config.js

--- React-Frontend-Code/vite.config.js
+++ React-Frontend-Code/vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: true // सभी होस्ट्स को अलाऊ करेगा
-  }
 })
